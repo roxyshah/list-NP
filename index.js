a3d5647394a86d30e0d3dcb8806b5f16488efda5
Roxanna Shahpouri <roxannaShahpouri@gmail.com>
2019-07-03 14:12:05 -0400

    requirements almost met

--- index.js
+++ index.js
@@ -1,36 +1,62 @@
 'use strict';
 
-function getUserRepos (username) {
-    fetch('https://api.github.com/users/' + username +'/repos')
-        .then(function(response) {
-            return response.json();
-        })
-        .then(function(responseJson) {
-            return displayResults(responseJson);
-        })
-        .catch(error => alert('Something went wrong. Try again later.')); 
-}
+//https://developer.nps.gov/api/v1/parks?stateCode=CO&api_key=gcUGALBm35IkRAkYql7IJiKobuFvSkyu0PgUzTQF
+const apiKey = 'gcUGALBm35IkRAkYql7IJiKobuFvSkyu0PgUzTQF'; 
+const searchURL = 'https://developer.nps.gov/api/v1/parks';
+
+function formatQueryParams(params) {
+    const queryItems = Object.keys(params)
+      .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
+    return queryItems.join('&');
+  }
 
 //returns an array. inside array are repo objects (each repo the username has)
 //repo object will have name variable we could use
 function displayResults(responseJson) {
     console.log(responseJson);
-    for(let i = 0; i < responseJson.length; i++) {
-        console.log(responseJson[i].name);
-        const listItem = '<li><a href="' + responseJson[i].html_url + '" target="_blank">' 
-        + responseJson[i].name 
-        +'</a></li>';
+    for(let i = 0; i < responseJson.data.length; i++) {
+        console.log(responseJson.data[i].name);
+        const listItem = '<li><a href="' + responseJson.data[i].html_url + '" target="_blank">' 
+        + responseJson.data[i].name 
+        +'</a><p>' + responseJson.data[i].description + '</p></li>';
         $('#results-list').append(listItem);
     }
     $('#results').removeClass('hidden');
+}
+
+function renderParkList(query, userStateCode, maxResults=10) {
+    const params = {
+      api_key: apiKey,
+      q: query,
+      stateCode: userStateCode,
+      limit: maxResults
+    };
+    const queryString = formatQueryParams(params)
+    const url = searchURL + '?' + queryString;
+  
+    console.log(url);
+    
+  fetch(url)
+  .then(response => {
+    if (response.ok) {
+      return response.json();
+    }
+    throw new Error(response.statusText);
+  })
+  .then(responseJson => displayResults(responseJson))
+  .catch(err => {
+    $('#js-error-message').text(`Something went wrong: ${err.message}`);
+  });
 }
 
 function watchForm() {
   $('form').submit(event => {
     event.preventDefault();
     $('#results-list').empty();
-    const searchTerm = $('#searchUserHandle').val();
-    getUserRepos(searchTerm);
+    const searchTerm = $('#searchNatlParks').val();
+    const maxResults = $('#js-max-results').val();
+    const stateCode = $('#userStateCode').val();
+    renderParkList(searchTerm, stateCode, maxResults);
   });
 }
 
